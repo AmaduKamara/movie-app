baed412367ad509086827f1e4fc7938b0465e82f
Amkam <amaduskamara@gmail.com>
2022-06-17 13:07:27 +0000

    Remove movie object comment

--- src/App.js
+++ src/App.js
@@ -5,15 +5,6 @@ import MovieCard from "./components/MovieCard";
 import searchIcon from "./search.svg";
 const API_URL = "https://www.omdbapi.com?apikey=152bf4fe";
 
-// const movie1 = {
-//   Title: "The Avengers",
-//   Year: "2012",
-//   imdbID: "tt0848228",
-//   Type: "movie",
-//   Poster:
-//     "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
-// };
-
 const App = () => {
   const [movies, setMovies] = useState([]);
 
