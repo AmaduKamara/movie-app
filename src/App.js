910053c5c027d26e4b7ed30ac14e8f6329fcc836
Amkam <amaduskamara@gmail.com>
2022-06-17 12:53:14 +0000

    Fetch movies, use single movie obj and render on the screen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import { useEffect } from "react";
+import "./App.css";
+
+import searchIcon from "./search.svg";
 const API_URL = "https://www.omdbapi.com?apikey=152bf4fe";
 
+const movie1 = {
+  Title: "The Avengers",
+  Year: "2012",
+  imdbID: "tt0848228",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+};
+
 const App = () => {
+  // const []
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
@@ -12,8 +25,39 @@ const App = () => {
   }, []);
 
   return (
-    <div className="App">
-      <h1>Movie App</h1>
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search for movies"
+          value="Avengers"
+          onChange={() => {}}
+        />
+        <img src={searchIcon} alt="search" onChange={() => {}} />
+      </div>
+
+      <div className="container">
+        <div className="movie">
+          <div>
+            <p>{movie1.Year}</p>
+          </div>
+          <div>
+            <img
+              src={
+                movie1.Poster !== "N/A"
+                  ? movie1.Poster
+                  : "https://vie.placeholder.com/400"
+              }
+              alt={movie1.Title}
+            />
+          </div>
+          <div>
+            <span>{movie1.Type}</span>
+            <h3>{movie1.Title}</h3>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
