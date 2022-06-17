d3fad0f59fd77fd0b66046da5a63d0692c21c0c7
Amkam <amaduskamara@gmail.com>
2022-06-17 12:34:50 +0000

    Fetch movies from OMDB

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import { useEffect } from "react";
 const API_URL = "https://www.omdbapi.com?apikey=152bf4fe";
 
 const App = () => {
-  useEffect(() => {}, []);
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data);
+  };
+  useEffect(() => {
+    searchMovies("Avengers");
+  }, []);
 
   return (
     <div className="App">
